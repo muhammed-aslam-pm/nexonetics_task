edabc4462a2d66fc0cbedf3a96dd76e11d240410
muhammed-aslam-pm <muhammedaslampm707@gmail.com>
2024-03-15 17:24:45 +0530

    firebase initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:nexonetics_task/firebase_options.dart';
 import 'package:nexonetics_task/view/home_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -5,6 +5,26 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Gallery"),
+      ),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          mainAxisSpacing: 5,
+          crossAxisSpacing: 5,
+        ),
+        itemCount: 20,
+        itemBuilder: (context, index) => Image.network(
+          "https://www.bing.com/th?id=ORES.MMS_2fe0cde68c7abb2aea260d32b73cdb3f",
+          fit: BoxFit.cover,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.upload),
+        onPressed: () {},
+      ),
+    );
   }
 }
